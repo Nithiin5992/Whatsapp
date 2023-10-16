cd0b299d6c8e1d39898adabe73eaa5e259f59d89
Nithiin5992 <nithinkumar2513@gmail.com>
2023-10-16 18:37:03 +0530

    interacting frontend with backend

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const signupRoutes=require('./routes/signup');
 const chatRoutes=require('./routes/chat')
 const groupRoutes=require('./routes/group')
 const sequelize = require('./util/database');
+const path =require("path")
 const cors=require("cors")
 
 app.use(bodyParser.json({ extended: false }));
@@ -12,6 +13,10 @@ app.use(cors())
 app.use(signupRoutes)
 app.use(chatRoutes)
 app.use(groupRoutes)
+app.use((req, res) => {
+
+    res.sendFile(path.join(__dirname, 'public', req.url))
+})
 sequelize.sync()
     .then((responce) => {
         app.listen(8080);
